be9eb1ee8dd93cfce39e2ede7eabb97be0868dbf
JonathanChiii <chiqingyuan@hotmail.com>
2022-06-16 19:53:54 -0400

    added card view

--- src/App.js
+++ src/App.js
@@ -1,102 +1,35 @@
 import { Component } from 'react';
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import { createNote, deleteNote } from './graphql/mutations';
-import { listNotes } from './graphql/queries';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import Listings from './components/ListingsComponent';
+import { ITEMS } from './shared/items' ;
 
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1';
-import awsExports from './aws-exports';
-
-Amplify.configure(awsExports);
-
-class AddNote extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
-  }
-
-  handleChange = (event) => {
-    this.setState({ text: event.target.value });
-  }
-
-  handleClick = () => {
-    this.props.addNote(this.state);
-    this.setState({ text: '' });
-  }
-
-  render() {
-    return (
-      <div style={styles.form}>
-        <input
-          value={this.state.text}
-          onChange={this.handleChange}
-          placeholder="New Note"
-          style={styles.input}
-        />
-        <button onClick={this.handleClick} style={styles.addButton}>Add Note</button>
-      </div>
-    );
-  }
-}
-
-class NotesList extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.notes.map(note =>
-          <div key={note.id} style={styles.note}>
-            <p>{note.text}</p>
-            <button onClick={() => { this.props.deleteNote(note) }} style={styles.deleteButton}>x</button>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
 
 class App extends Component {
+
   constructor(props) {
     super(props);
-    this.state = { notes: [] };
-  }
 
-  async componentDidMount() {
-    var result = await API.graphql(graphqlOperation(listNotes));
-    this.setState({ notes: result.data.listNotes.items });
-  }
-
-  deleteNote = async (note) => {
-    const id = {
-      id: note.id
+    this.state = {
+      items : ITEMS
     };
-    await API.graphql(graphqlOperation(deleteNote, { input: id }));
-    this.setState({ notes: this.state.notes.filter(item => item.id !== note.id) });
-  }
-
-  addNote = async (note) => {
-    var result = await API.graphql(graphqlOperation(createNote, { input: note }));
-    this.state.notes.push(result.data.createNote);
-    this.setState({ notes: this.state.notes });
+    
   }
 
   render() {
     return (
-      <div style={styles.container}>
-        <h1>Notes App</h1>
-        <AddNote addNote={this.addNote} />
-        <NotesList notes={this.state.notes} deleteNote={this.deleteNote} />
-        <AmplifySignOut />
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href="http://google.com"> YeeBay </NavbarBrand>
+          </div>
+        </Navbar>
+        <Listings items = {this.state.items} />
       </div>
     );
   }
 }
 
-export default withAuthenticator(App);
-
-const styles = {
-  container: { width: 480, margin: '0 auto', padding: 20 },
-  form: { display: 'flex', marginBottom: 15 },
-  input: { flexGrow: 2, border: 'none', backgroundColor: '#ddd', padding: 12, fontSize: 18 },
-  addButton: { backgroundColor: 'black', color: 'white', outline: 'none', padding: 12, fontSize: 18 },
-  note: { display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontSize: 22, marginBottom: 15 },
-  deleteButton: { fontSize: 18, fontWeight: 'bold' }
-}
+export default App;
+/* this app is an export from App.js file */
